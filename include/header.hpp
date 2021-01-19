0462a83f2f796ab97d22456cf7c14182cbc291a4
Tasia11 <61164926+Tasia11@users.noreply.github.com>
2021-01-19 22:56:52 +0300

    Update header.hpp

--- include/header.hpp
+++ include/header.hpp
@@ -3,4 +3,28 @@
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <exception>
+#include <boost/filesystem.hpp>
+#include <utility>
+
+#define bf boost::filesystem
+
+class FileSort
+{
+ private:
+  bf::path ftp_path;
+  std::map<std::string, std::map<int, std::pair<int, int>>> Brokers;
+  size_t NumBrokers;
+ public:
+  explicit FileSort(const bf::path& ftp_path);
+  std::map<std::string, std::map<int, std::pair<int, int>>> GetBrokers();
+
+  size_t GetNumBrokers();
+  void filer(const bf::path &path);
+  ~FileSort();
+};
 #endif // INCLUDE_HEADER_HPP_
