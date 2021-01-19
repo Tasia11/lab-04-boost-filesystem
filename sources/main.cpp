8393d92e684c8c4057a371ae1e4d078f8cf83cc7
Tasia11 <61164926+Tasia11@users.noreply.github.com>
2021-01-19 23:01:31 +0300

    Create main.cpp

---
+++ sources/main.cpp
@@ -0,0 +1,14 @@
+
+#include <header.hpp>
+
+int main(int argc, char *argv[]) {
+  bf::path ftp_path;
+  if (argc < 2)
+    ftp_path = "../";
+  else
+    ftp_path = std::string(argv[1]);
+
+  FileSort A(ftp_path);
+  std::cout<< A.GetBrokers().size();
+  return 0;
+}
